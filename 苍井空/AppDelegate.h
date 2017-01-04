ff78ba2c68cf4e2a5d4eda6109ee7ea54012cdc8
liuyi123 <296114106@qq.com>
2017-01-04 22:40:36 +0800

    Initial Commit

---
+++ 苍井空/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  苍井空
+//
+//  Created by liuyi on 17/1/4.
+//  Copyright © 2017年 liuyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 苍井空/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  苍井空
+//
+//  Created by liuyi on 17/1/4.
+//  Copyright © 2017年 liuyi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
